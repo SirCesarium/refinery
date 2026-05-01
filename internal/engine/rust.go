5faa0b4f8eca6b9a700bcef63d82d4892455d59a
SirCesarium <cesarium3600@gmail.com>
2026-05-01 11:06:33 -0400

    fix: refactor target triple determination and improve Linux setup for aarch64

--- internal/engine/rust.go
+++ internal/engine/rust.go
@@ -18,29 +18,39 @@ func (e *RustEngine) ID() string {
 func (e *RustEngine) Build(cfg *config.Config, art *config.ArtifactConfig, opts BuildOptions) error {
 	var targetTriple string
 
-	if opts.OS == "darwin" {
+	switch opts.OS {
+	case "darwin":
 		targetTriple = fmt.Sprintf("%s-apple-darwin", opts.Arch)
-	} else {
+	case "windows":
+		vendor := "pc"
+		targetTriple = fmt.Sprintf("%s-%s-windows", opts.Arch, vendor)
+		if opts.ABI != "" {
+			targetTriple = fmt.Sprintf("%s-%s", targetTriple, opts.ABI)
+		}
+	default:
 		targetTriple = fmt.Sprintf("%s-unknown-%s", opts.Arch, opts.OS)
 		if opts.ABI != "" {
 			targetTriple = fmt.Sprintf("%s-%s", targetTriple, opts.ABI)
 		}
 	}
 
-	if runtime.GOOS == "linux" && opts.OS == "linux" {
-		if opts.Arch == "aarch64" {
-			exec.Command("sudo", "apt-get", "update").Run()
-			exec.Command("sudo", "apt-get", "install", "-y", "gcc-aarch64-linux-gnu").Run()
-			os.Setenv("CARGO_TARGET_AARCH64_UNKNOWN_LINUX_GNU_LINKER", "aarch64-linux-gnu-gcc")
-			os.Setenv("CARGO_TARGET_AARCH64_UNKNOWN_LINUX_MUSL_LINKER", "aarch64-linux-gnu-gcc")
+	if runtime.GOOS == "linux" && opts.OS == "linux" && opts.Arch == "aarch64" {
+		if _, err := exec.LookPath("aarch64-linux-gnu-gcc"); err != nil {
+			if os.Geteuid() == 0 || os.Getenv("GITHUB_ACTIONS") == "true" {
+				exec.Command("apt-get", "update").Run()
+				exec.Command("apt-get", "install", "-y", "gcc-aarch64-linux-gnu").Run()
+			}
 		}
+
+		os.Setenv("CARGO_TARGET_AARCH64_UNKNOWN_LINUX_GNU_LINKER", "aarch64-linux-gnu-gcc")
+		os.Setenv("CARGO_TARGET_AARCH64_UNKNOWN_LINUX_MUSL_LINKER", "aarch64-linux-gnu-gcc")
 	}
 
 	if runtime.GOOS == "darwin" && opts.OS == "darwin" {
-		if opts.Arch == "aarch64" || opts.Arch == "x86_64" {
+		if _, err := os.Stat("/Library/Developer/CommandLineTools/SDKs/MacOSX.sdk"); err == nil {
 			os.Setenv("SDKROOT", "/Library/Developer/CommandLineTools/SDKs/MacOSX.sdk")
-			os.Setenv("MACOSX_DEPLOYMENT_TARGET", "11.0")
 		}
+		os.Setenv("MACOSX_DEPLOYMENT_TARGET", "11.0")
 	}
 
 	setupCmd := exec.Command("rustup", "target", "add", targetTriple)
@@ -63,6 +73,7 @@ func (e *RustEngine) Build(cfg *config.Config, art *config.ArtifactConfig, opts 
 
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
+	cmd.Env = os.Environ()
 
 	return cmd.Run()
 }
